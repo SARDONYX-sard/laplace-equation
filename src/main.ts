891d519b18da8bd9a015690d8d32c1e3810cc472
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-11-20 03:51:27 +0900

    feat: :poop: feat: add python code to `main.ts`

--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,51 @@ Solution of the 2-D Laplace equation
 Reference: https://www.ohmsha.co.jp/book/9784274221705/
 */
 
-import { arange } from "numjs";
-import { range } from "./utils/range";
-import { clone } from "./utils/clone";
+# -- Main execution part --
 
-function iteration(u: number[][]) {
-  /*
-    Calculate u_ij with Finite Difference Methods (FDM)
-    Boundary Condition: D = [0, 1] * [0, 1] (100 * 100 mesh)
-     */
-  const u_next = clone(range({ stop: NX }), NY); // Initialize
+u = [[0 for i in range(Nx)] for j in range(Ny)]  # Initialize
 
-  const loopObj = { start: 1, stop: NX - 1 };
+for j in range(Ny):
+    u[0][j] = math.sin(2 * math.pi * j / (Ny - 1))  # u_0,j = sin(2πj/100)
 
-  for (let i of range(loopObj)) {
-    for (let j of range(loopObj)) {
-      u_next[i][j] = (u[i][j - 1] + u[i - 1][j] + u[i + 1][j] + u[i][j + 1]) / 4;
-    }
-  }
-  // Update u_ij
-  for (let i of range(loopObj)) {
-    for (let j of range(loopObj)) {
-      u[i][j] = u_next[i][j];
-    }
-  }
+# Jacobi method
+for i in range(lim):
+    iteration(u)
 
-  // End of the function iteration()
-}
+# -- Output solution --
+# print(u)
+
+f = open("output/output-solution-u.csv", mode="w")
+print("x,", "y,", "u", file=f)
+
+for i in range(Nx - 1):
+    for j in range(Ny - 1):
+        hx = i / 100      # stride length for Nx
+        hy = j / 100      # stride length for Ny
+        print(hx, ",", hy, ",", u[i][j], file=f)
+
+# Setting the range and interval
+x = np.arange(0, Nx)
+y = np.arange(0, Ny)
+
+# Mesh drawing settings
+X, Y = np.meshgrid(x, y)
+U = np.array(u)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+
+# Plot
+ax.plot_wireframe(X, Y, U)
+ax.plot_surface(X, Y, U, cmap = "jet")
+
+# Set axis and title label
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("u(x, y)")
+
+ax.set_title("2-D Laplace equation", c="darkred",
+            size="large", style="italic")
+
+plt.show()
+# End of 2D-laplace.py
